7f7016e87ce1a4b401aacec6c9158c7441043a19
musamolla3131 <musamolla3131@gmail.com>
2022-08-23 18:10:05 +0600

    Calculator ar khel khatam. Akhob sudhut playerder addmition baki hurre(❁´◡❁)

--- js/index.js
+++ js/index.js
@@ -1,13 +1,43 @@
 
 
+document.getElementById('playerButton').addEventListener('click', function (event) {
+    const player = event.target.innerText;
+    if (player === 'SELECT') { 
+        
+        document.getElementById(player).disabled = true;
+        // const playerList = document.getElementById('orderdList');  
+        // playerList.length = 0;
+        // const option = document.createElement('option');
+        // option.text = 'SELECT';
+        // playerList.add(option);
+    } else {
+        const playerList = document.getElementById('orderdList');  
+        playerList.length = 0;
+        const option = document.createElement('option');
+        option.text = 'SELECT';
+        playerList.add(option);
+        for (let i = 0; i < players.length; i++) {
+            if (players[i].position === player) {
+                const option = document.createElement('option');
+                option.text = players[i].name;
+                playerList.add(option);
+            }
+        }
+    }
+    
+ });
+
+
+
 // Calculation
 document.getElementById("calculate").addEventListener("click", function () { 
+    const player = document.getElementById("orderdList");
+    const playerNumbers = player.childElementCount;
     const inputValue = getElementValueById("perPlayer", "Per Player");
-    
     const expenses = document.getElementById("playerExpenses");
     const expensesInt = parseInt(expenses.innerText);
     if(!isNaN(inputValue)) {
-        expenses.innerText = inputValue * 5;
+        expenses.innerText = inputValue * playerNumbers;
     }
 });
 
